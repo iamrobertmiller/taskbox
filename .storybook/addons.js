9c85c0d8988054abd2d9440542446f7e2a6173e2
Robert Miller <robert.miller@geniussports.com>
2019-10-08 09:57:08 +1100

    Testing and Addons

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -1,2 +1,5 @@
-import '@storybook/addon-actions/register';
-import '@storybook/addon-links/register';
+// .storybook/addons.js
+
+import "@storybook/addon-actions/register";
+import "@storybook/addon-knobs/register";
+import "@storybook/addon-links/register";
--- .storybook/config.js
+++ .storybook/config.js
@@ -1,14 +1,15 @@
 // .storybook/config.js
 
-import { configure } from '@storybook/react'
-import requireContext from 'require-context.macro'
+import { configure } from "@storybook/react";
+import requireContext from "require-context.macro";
+import "storybook-chromatic";
 
-import '../src/index.css'
+import "../src/index.css";
 
-const req = requireContext('../src/components', true, /\.stories\.js$/)
+const req = requireContext("../src/components", true, /\.stories\.js$/);
 
 function loadStories() {
-  req.keys().forEach(filename => req(filename))
+  req.keys().forEach(filename => req(filename));
 }
 
-configure(loadStories, module)
+configure(loadStories, module);
